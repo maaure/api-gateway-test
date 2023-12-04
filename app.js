d7be132022131f7072d5601a0f73e2b8bf443170
Pedro Maure Frutuoso de Andrade <20221014040013@ifrn.local>
2023-12-04 07:56:18 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const axios = require("axios");
+const express = require("express");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const app = express();
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get("/raposa", async (req, res) => {
+  try {
+    const response = await axios.get("https://randomfox.ca/floof/");
+    const { message, link } = response.data;
+    res.json({ mensagem: "Você solicitou por uma raposa", imagem: link });
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar informações da raposa" });
+  }
+});
+
+app.get("/pato", async (req, res) => {
+  try {
+    const response = await axios.get("https://random-d.uk/api/quack");
+    const { message, url } = response.data;
+    res.json({ mensagem: "Você solicitou por um pato", imagem: url });
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar informações do pato" });
+  }
+});
+
+app.get("/cachorro", async (req, res) => {
+  try {
+    const response = await axios.get("https://random.dog/woof.json");
+    const { message, url } = response.data;
+    res.json({ mensagem: "Você solicitou por um cachorro", imagem: url });
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar informações do cachorro" });
+  }
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
